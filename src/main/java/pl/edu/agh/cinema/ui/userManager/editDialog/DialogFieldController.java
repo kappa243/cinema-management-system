81441b4cf2252b382ac6d60f1820378165dd9ee3
Karol Śliwa <405159@student.agh.edu.pl>
2022-12-28 16:42:57 +0100

    password validation

--- src/main/java/pl/edu/agh/cinema/ui/userManager/editDialog/DialogFieldController.java
+++ src/main/java/pl/edu/agh/cinema/ui/userManager/editDialog/DialogFieldController.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import pl.edu.agh.cinema.model.user.Role;
 
+
 @Component
 @Scope("prototype")
 public class DialogFieldController {
@@ -32,16 +33,24 @@ public class DialogFieldController {
     public Label warningMessage;
 
     public boolean validateInput(boolean updatePassword) {
-        // TODO - implement password validation (contains at least one digit, one uppercase letter, one lowercase letter, one special character, set minimal length)
-        // TODO - implement update password only if updatePassword true
         // TODO - implement is role selected validation
-        if (firstName.getText().matches(".*\\d.*") || firstName.getText().isEmpty()) {
-            warningMessage.setText("First name should contain only letters!");
+        if (!firstName.getText().matches("[A-Z]{1}[a-z]+") || firstName.getText().isEmpty()) {
+            warningMessage.setText("First name should contain only letters and start with uppercase!");
             return false;
         }
-        else if (lastName.getText().matches(".*\\d.*") || lastName.getText().isEmpty()) {
-            warningMessage.setText("Last name should contain only letters!");
+        else if (!lastName.getText().matches("[A-Z]{1}[a-z]+") || lastName.getText().isEmpty()) {
+            warningMessage.setText("Last name should contain only letters! and start with uppercase");
             return false;
+        }
+        else if (updatePassword) {
+            String passwd = password.getText();
+            if (passwd.length() < 8 || !passwd.matches(".*[!@#$%&*(),._+=|<>?{}\\[\\]~-].*")
+                    || !passwd.matches(".*[A-Z].*")
+                    || !passwd.matches(".*\\d.*")) {
+                warningMessage.setText("Password should contain at least 8 characters, including at least 1 uppercase" +
+                        " letter, 1 digit and 1 special character!");
+                return false;
+            }
         }
         else if (!email.getText().matches("^[\\w.]+@([\\w-]+.)+[\\w-]{2,4}$") || email.getText().isEmpty()) {
             warningMessage.setText("Wrong email address!");
